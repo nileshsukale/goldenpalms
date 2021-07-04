6a310ddd976bf4b60001dd4c5bc26b05caead0ab
Nilesh <nilesh.sukale@gmai.com>
2021-07-04 18:46:51 +0530

    my grolden papls project

---
+++ src/test/java/com/golden/GoldenPlams/GoldenPlamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golden.GoldenPlams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenPlamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
